f1360f81222aa398c961d2491bbfc6455c4570c5
Yurii <borlok@bk.ru>
2022-12-18 20:42:49 +0300

    add gateway

--- eureka-client/src/main/java/com/borlok/eurekaclient/controller/Controller.java
+++ eureka-client/src/main/java/com/borlok/eurekaclient/controller/Controller.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/main")
 public class Controller {
 
     @GetMapping("/test")
     public String test() {
-        return "hello world!!!";
+        return "LET'S CODE!!!";
     }
 }
